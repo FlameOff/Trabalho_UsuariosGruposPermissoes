7d0a6ac8b1099631be87d9209d37487e6140cf10
FlameDev <88896453+FlameOff@users.noreply.github.com>
2024-11-01 11:54:43 -0300

    Update trabalho.sh

--- trabalho.sh
+++ trabalho.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
-#1)
-sudo groupadd aluno
-sudo groupadd professor
-sudo groupadd gerente
+1-
+sudo addgroup aluno
+sudo addgroup professor
+sudo addgroup gerente
 
-# 2)
-sudo mkdir -p /srv
+2-
+sudo mkdir /srv
 sudo chown root:gerente /srv
-sudo chmod 750 /srv
 
-# 3)
+3-
 sudo mkdir /srv/alunos
 sudo mkdir /srv/professores
 sudo chown root:aluno /srv/alunos
 sudo chown root:professor /srv/professores
 
-# 4)
-sudo chmod 750 /srv
+4-
+sudo chmod 751 /srv
 sudo chmod 770 /srv/alunos
 sudo chmod 770 /srv/professores
 
-# 5)
-sudo useradd -m -d /srv/professores/bruno -g professor --gecos "" --disabled-login bruno
+5-
+sudo adduser --ingroup professor --home /srv/professores/bruno --gecos "" --disabled-login bruno
 
-# 6)
+6-
 sudo usermod -aG gerente bruno
 
-# 7)
-sudo useradd -m -d /srv/alunos/joao -g aluno --gecos "" --disabled-login joao
-sudo useradd -m -d /srv/alunos/maria -g aluno --gecos "" --disabled-login maria
+7-
+sudo adduser --ingroup aluno --home /srv/alunos/maria --gecos "" --disabled-login maria
+sudo adduser --ingroup aluno --home /srv/alunos/joao --gecos "" --disabled-login joao
 
-# 8)
-sudo chage -E 2024-12-31 joao
-sudo chage -E 2024-12-31 maria
-sudo chage -M 90 joao
-sudo chage -M 90 maria
+8-
+sudo usermod -e 2024-12-31 -f 90 joao
+sudo usermod -e 2024-12-31 -f 90 maria
